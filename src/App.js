b0e5b7cc5905ab23ab88b38ed8a744fafee67992
Khairat Adam <adamkhairat0@gmail.com>
2021-10-06 20:27:40 +0100

    adding the files

---
+++ src/App.js
@@ -0,0 +1,18 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+      <h1>You thirsty?</h1>
+        <article>
+          <p>Explore local breweries with just one click and stirred by starlight across the centuries light years great turbulent clouds circumnavigated paroxysm of global death.</p>
+          <a href="#breweries">Browse Breweries</a>
+        </article>
+      </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Breweries.js
@@ -0,0 +1,38 @@
+import React from 'react';
+import './Breweries.css';
+import first from './first.jpg';
+import second from './second.jpg';
+import fourth from './fourth.jpg';
+
+const Breweries = () => {
+    return (
+        <div className= "breweries" id="breweries">
+            <ul>
+                <li>
+                    <figure>
+                        <img src={first} alt="first" />
+                        <figcaption><h3>Billions upon billions</h3></figcaption>
+                    </figure>
+                    <p>Made in the interiors of collapsing star stuff harvesting star light venture billions Drake Equation brain is the seed of intelligence?</p>
+                    <a href="!#">Visit Website</a>
+
+                    <figure>
+                        <img src={second} alt="second" />
+                        <figcaption><h3>Billions upon billions</h3></figcaption>
+                    </figure>
+                    <p>Made in the interiors of collapsing star stuff harvesting star light venture billions Drake Equation brain is the seed of intelligence?</p>
+                    <a href="!#">Visit Website</a>
+
+                    <figure>
+                        <img src={fourth} alt="fourth" />
+                        <figcaption><h3>Billions upon billions</h3></figcaption>
+                    </figure>
+                    <p>Made in the interiors of collapsing star stuff harvesting star light venture billions Drake Equation brain is the seed of intelligence?</p>
+                    <a href="!#">Visit Website</a>
+                </li>
+            </ul>
+        </div>
+    )
+}
+
+export default Breweries;
